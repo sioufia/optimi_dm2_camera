fcde1849188ce0b78b70faccccd755540149543c
Yoann Gauthier <yoann.gauthier@nebulae.co>
2018-02-05 14:12:08 +0100

    Seems to work (yet untested) but very slow : around 3 sec / artwork to check constraints, for 5000 artworks ~4 hours

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from pyscipopt import Model, quicksum
 from parser import parser, get_grid_dimensions
+from datetime import datetime
 
 model = Model("Cameras")
 # cams_caracs: ((a,b),(a',b')) a is the capacity radius and b the cost of the camera
@@ -19,13 +20,14 @@ class Disk(object):
     def isInDisk(self, x, y):
         return (x - self.__x) ** 2 + (y - self.__y) ** 2 <= self.__radius ** 2
 
-def get_number_of_cams_in_radius(x, y, cams, radius):
-    n = 0
+
+def get_cams_in_radius(x, y, vars, cams, radius):
+    cams_in_radius = []
     disk = Disk(x, y, radius)
-    for cam in cams:
+    for index, cam in enumerate(cams):
         if disk.isInDisk(cam[0], cam[1]):
-            n += 1
-    return n
+            cams_in_radius.append(vars[index])
+    return cams_in_radius
 
 # boolean variables corresponding to the cameras, corresponding to the pattern
 # [(x, y, radius), ...]
@@ -42,10 +44,21 @@ for var in cams:
 # need to filter the cams to ensure they are close from some artwork (maybe use isInDisk util ?)
 # need to add constraints to ensure all artworks are covered
 
-for artwork in artworks:
-    CONS = get_number_of_cams_in_radius(artwork[0], artwork[1], cams, cams_caracs[0][0]) + get_number_of_cams_in_radius(artwork[0], artwork[1], cams, cams_caracs[1][0]) >= 1
-    print(CONS)
-    model.addCons(CONS)
+for index, artwork in enumerate(artworks):
+    if index % 100 == 0:
+        print(index)
+    # a camera must be around in a dertermined radius
+    t1 = datetime.now()
+    cams_in_radius_1 = get_cams_in_radius(artwork[0], artwork[1], vars, cams, cams_caracs[0][0])
+    t2 = datetime.now()
+    cams_in_radius_2 = get_cams_in_radius(artwork[0], artwork[1], vars, cams, cams_caracs[1][0])
+    t3 = datetime.now()
+    model.addCons(quicksum(var for var in cams_in_radius_1) + quicksum(var for var in cams_in_radius_2) >= 1)
+    t4 = datetime.now()
+    print("D3:", t4 - t3, "D2:", t3 - t2, "D1:", t2 - t1,)
+    # CONS = get_number_of_cams_in_radius(artwork[0], artwork[1], cams, cams_caracs[0][0]) + get_number_of_cams_in_radius(artwork[0], artwork[1], cams, cams_caracs[1][0]) >= 1
+    # print(CONS)
+    # model.addCons(CONS)
 
 
 cams1 = [cam for cam in cams if cam[2] == cams_caracs[0][0]]
